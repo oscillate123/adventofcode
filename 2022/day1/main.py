4b8fd23fee52d3e7ba8e30c305acb65123bb324d
oscillate123 <oscillate123>
2022-12-01 19:44:35 +0100

    part 2

--- 2022/day1/main.py
+++ 2022/day1/main.py
@@ -8,6 +8,9 @@ class Elf():
         self.items = items
         self.calories = self.total_calories_carried()
 
+    def __repr__(self) -> str:
+        return repr((f'id: {self.id}', f'kcal: {self.calories}', f'items: {len(self.items)}'))
+
     def total_calories_carried(self):
         _total = 0
         for item in self.items:
@@ -26,14 +29,8 @@ class ElfOnAShelf:
             _temp_elf = Elf(id=index, items=items)
             self.elves.append(_temp_elf)
 
-    def find_elf_with_most_calories(self):
-        strongest_elf = Elf(id=99999999)
-
-        for elf in self.elves:
-            if elf.calories > strongest_elf.calories:
-                strongest_elf = elf
-
-        self.biggus_elfus = strongest_elf
+    def sort_elves_by_calories(self):
+        self.elves = sorted(self.elves, key=lambda elf: elf.calories)
 
 
 if __name__ == "__main__":
@@ -46,8 +43,10 @@ if __name__ == "__main__":
     filtrated_data = filtrate_calories_data(file_content)
 
     project.load_calories_data(filtrated_data)
+    project.sort_elves_by_calories()
 
-    project.find_elf_with_most_calories()
+    print(project.elves[-1]) # part 1
 
-    print(project.biggus_elfus.id)
-    print(project.biggus_elfus.calories)
+    print(project.elves[-1:-4:-1])
+    top3_tot = sum(elf.calories for elf in project.elves[-1:-4:-1])
+    print(top3_tot) # part 2
